7e49d7773e95aab17098a1264523b4d60718f0e9
Dell Pc <mudasir9757@gmail.com>
2023-03-08 21:18:03 +0500

    Required changes add

--- student.rb
+++ student.rb
@@ -3,8 +3,8 @@ require_relative 'person'
 class Student < Person
   attr_accessor :classroom
 
-  def initialize(id, classroom, name = 'Unknown', age = nil, parent_permission = 'true')
-    super(id, name, age, parent_permission)
+  def initialize(classroom, name = 'Unknown', age = nil, parent_permission = 'true')
+    super(name, age, parent_permission)
     @classroom = classroom
   end
 
--- teacher.rb
+++ teacher.rb
@@ -3,8 +3,8 @@ require_relative 'person'
 class Teacher < Person
   attr_accessor :specialization
 
-  def initialize(specialization, id, name = 'Unknown', age = nil, parent_permission = 'true')
-    super(id, name, age, parent_permission)
+  def initialize(specialization, name = 'Unknown', age = nil, parent_permission = 'true')
+    super(name, age, parent_permission)
     @specialization = specialization
   end
 
