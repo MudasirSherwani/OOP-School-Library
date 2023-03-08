3224d827520b64ecced3dd1f45f43d8f48d4639d
Dell Pc <mudasir9757@gmail.com>
2023-03-09 01:01:39 +0500

    Add Class NameDecorator

---
+++ name_decorator.rb
@@ -0,0 +1,12 @@
+require_relative 'nameable'
+
+class NameDecorator < Nameable
+  def initialize(nameable)
+    super()
+    @nameable = nameable
+  end
+
+  def correct_name
+    @nameable.correct_name
+  end
+end
