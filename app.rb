49d189e8825dec1cca174e10414f440bb0f10428
Dell Pc <mudasir9757@gmail.com>
2023-03-09 14:47:55 +0500

    fix linter

--- app.rb
+++ app.rb
@@ -7,5 +7,3 @@ capitalized_person = CapitalizeDecorator.new(person)
 puts capitalized_person.correct_name
 capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
 puts capitalized_trimmed_person.correct_name
-
-
