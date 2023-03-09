f6cc685e21e1b5501fdeffad0715f892e196e556
Dell Pc <mudasir9757@gmail.com>
2023-03-09 15:03:47 +0500

    Add uppercase feature

--- capitalize_decorator.rb
+++ capitalize_decorator.rb
@@ -2,6 +2,6 @@ require_relative 'name_decorator'
 
 class CapitalizeDecorator < NameDecorator
   def correct_name
-    @nameable.correct_name.capitalize
+    @nameable.correct_name.upcase
   end
 end
