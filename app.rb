bdf438bd7d551a5d0470e2665b2c275d2ceef2e0
Dell Pc <mudasir9757@gmail.com>
2023-03-09 14:50:48 +0500

    fix linter error

--- app.rb
+++ app.rb
@@ -6,4 +6,4 @@ puts person.correct_name
 capitalized_person = CapitalizeDecorator.new(person)
 puts capitalized_person.correct_name
 capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
-puts capitalized_trimmed_person.correct_name
+print capitalized_trimmed_person.correct_name
