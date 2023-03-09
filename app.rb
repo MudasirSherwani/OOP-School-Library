df11e88468c8a51d6064e791becd8e7a7971a507
Dell Pc <mudasir9757@gmail.com>
2023-03-09 15:14:29 +0500

    Add required changes

--- app.rb
+++ app.rb
@@ -5,5 +5,5 @@ person = Person.new(22, 'maximilianus')
 puts person.correct_name
 capitalized_person = CapitalizeDecorator.new(person)
 puts capitalized_person.correct_name
-capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
+capitalized_trimmed_person = TrimmerDecorator.new(person)
 print capitalized_trimmed_person.correct_name
