cf340027860b0dcf3af4fe98bd818323d39022f6
Dell Pc <mudasir9757@gmail.com>
2023-03-10 21:50:26 +0500

    Define Person (Create and list)

--- app.rb
+++ app.rb
@@ -11,6 +11,7 @@ class App
 
   def initialize
     @books = []
+    @people = []
   end
 
   def list_books
@@ -41,4 +42,64 @@ class App
     puts 'Book created successfully'
   end
 
+  
+  def list_people
+    person_data = ''
+    @people.each do |p|
+      p.instance_variables.each do |val|
+        value = p.instance_variable_get(val)
+        val = val.to_s.delete('@')
+        val = val.capitalize
+        person_data += " #{val}: #{value} " unless val.include?('Rentals') or val.include?('Classroom')
+      end
+      person_data += "\n"
+    end
+    if person_data == ''
+      puts 'No person Found !!'
+    else
+      puts person_data
+    end
+  end
+
+  def create_person
+    puts 'Choose an option student(1) or teacher(2)?'
+    choise = gets.chomp.to_i
+    return unless choise != 0
+
+    case choise
+    when 1
+      create_student
+    when 2
+      create_teacher
+    else
+      puts 'Invalid Input ( Enter 0 for Exit)'
+      create_person
+    end
+  end
+
+  
+  def create_student
+    puts 'Age: '
+    ag = gets.chomp
+    puts 'Name: '
+    nm = gets.chomp
+    puts 'Has parent permission? [Y/N]'
+    parent_permission = gets.chomp.upcase == 'Y'
+    student = Student.new(ag, nm, parent_permission)
+    @people.push(student)
+    puts 'Student added  successfully'
+  end
+
+  def create_teacher
+    puts 'Age: '
+    ag = gets.chomp
+    puts 'Name: '
+    nm = gets.chomp
+    puts 'Specialization: '
+    specialization = gets.chomp
+    teacher = Teacher.new(ag, nm, specialization)
+    @people.push(teacher)
+    puts 'Teacher added successfully'
+  end
+  
 end
