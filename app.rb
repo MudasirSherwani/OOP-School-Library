333a759e74a47cc7902889fde7fb28f1b0b8ad4a
Dell Pc <mudasir9757@gmail.com>
2023-03-10 21:57:44 +0500

    define rentals (Create and list)

--- app.rb
+++ app.rb
@@ -12,6 +12,7 @@ class App
   def initialize
     @books = []
     @people = []
+    @rentals = []
   end
 
   def list_books
@@ -88,7 +89,7 @@ class App
     @people.push(student)
     puts 'Student added  successfully'
   end
-  
+
   def create_teacher
     puts 'Age: '
     ag = gets.chomp
@@ -101,5 +102,68 @@ class App
     puts 'Teacher added successfully'
   end
   
+  
+  def create_rental
+    puts 'Select a book from the list by No'
+    books_index
+    selected_book = gets.chomp.to_i
+    puts 'Select a person from the list by No'
+    people_index
+    selected_person = gets.chomp.to_i
+    print 'Date: '
+    date = gets.chomp
+    book = @books[selected_book]
+    person = @people[selected_person]
+    rental = Rental.new(date, book, person)
+    @rentals.push(rental)
+    puts 'Rental created successfully'
+  end
+
+  def list_rentals
+    rental_data = ''
+    print 'To see person rentals enter the person ID: '
+    id = gets.chomp.to_i
+    rental_data = 'Rented Books: '
+    @rentals.each do |rent|
+      person = rent.instance_variable_get(:@person)
+      person_id = person.instance_variable_get(:@id)
+      next unless person_id == id
+
+      book = rent.instance_variable_get(:@book)
+      title = book.instance_variable_get(:@title)
+      author = book.instance_variable_get(:@author)
+      rental_data += " Date: #{rent.date} Book: #{title} by Author: #{author} "
+      rental_data += "\n"
+    end
+    if rental_data == ''
+      puts 'No Rental Data Found !!'
+    else
+      puts rental_data
+    end
+  end
+
+  def books_index
+    @books.each_index do |book, index|
+      book_data = " #{index}: "
+      book.instance_variables.each do |val|
+        value = book.instance_variable_get(val)
+        val = val.to_s.delete('@')
+        book_data += " #{val}: #{value} " unless val.include?('rentals') or val.include?('classroom')
+      end
+      puts book_data
+    end
+  end
+
+  def people_index
+    @people.each_index do |person, index|
+      person_data = " #{index}: "
+      person.instance_variables.each do |val|
+        value = person.instance_variable_get(val)
+        val = val.to_s.delete('@')
+        person_data += " #{val}: #{value} " unless val.include?('rentals') or val.include?('classroom')
+      end
+      puts person_data
+    end
+  end
 
 end
