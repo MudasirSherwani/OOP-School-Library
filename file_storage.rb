b51282c47181673ed512806bfd85492ab12d9d64
Dell Pc <mudasir9757@gmail.com>
2023-03-16 18:02:03 +0500

    Define file save method

--- file_storage.rb
+++ file_storage.rb
@@ -1,6 +1,13 @@
 require 'json'
 
 class Storage
-def initialize()
+def initialize(file)
+    @file = file
 end
+
+def save(data)
+    maped_data = data.map(&:to_h)
+    File.write(file, JSON.generate(maped_data))
+end
+
 end
