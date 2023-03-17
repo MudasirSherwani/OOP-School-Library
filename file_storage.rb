4f8ff28d17bead800df6a76b5145f5de9d10f169
Dell Pc <mudasir9757@gmail.com>
2023-03-17 15:57:39 +0500

    Add method for validity in storage

--- file_storage.rb
+++ file_storage.rb
@@ -6,16 +6,32 @@ class Storage
   end
 
   def save(data)
-    maped_data = data.map(&:to_h)
-    File.write(@file, JSON.generate(maped_data))
+    if File.exist?(@file)
+      maped_data = data.map(&:to_h)
+      File.write(@file, JSON.generate(maped_data))
+    end
   end
 
   def load()
-    if File.exist?(@file)
+    if File.exist?(@file) && valid?
       r_data = File.read(@file)
-      JSON.parse(r_data)
+      result = JSON.parse(r_data)
     else
       []
     end
   end
+
+  def valid?()
+    begin
+    result = ''
+    if File.exist?(@file)
+      r_data = File.read(@file)
+    result = JSON.parse(r_data)
+    end
+    result.is_a?(Hash) || result.is_a?(Array)
+  rescue JSON::ParserError, TypeError
+    false
+  end
+end
+
 end
